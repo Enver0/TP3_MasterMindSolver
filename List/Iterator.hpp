9d4a4ecba557f856562034be0e3959ce4225b2e0
Gabriel Frenette-Lareault <gab.lar@hotmail.com>
2014-04-30 11:16:35 -0400

    Iterator.hpp completed
    Still untested

--- List/Iterator.hpp
+++ List/Iterator.hpp
@@ -2,29 +2,35 @@
 template <class TYPE>
 Iterator<TYPE>::Iterator(void)
 {
+
 }
 
 template <class TYPE>
 void Iterator<TYPE>::Next(void)
 {
+	this->current = current->Next;
 }
 
 template <class TYPE>
 void Iterator<TYPE>::Previous(void)
 {
+	this->current = Cell<TYPE>::Previous;
 }
 
 template <class TYPE>
 TYPE* Iterator<TYPE>::GetCurrentElement(void) const
 {
+	return current->Element;
 }
 
 template <class TYPE>
 Cell<TYPE>* Iterator<TYPE>::GetCurrent(void) const
 {
+	return current;
 }
 
 template <class TYPE>
 void Iterator<TYPE>::SetCurrent(Cell<TYPE>* _cell)
 {
+	this->current = _cell;
 }
